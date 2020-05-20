11a9910349499e1f5a4640312c100d029fe4815d
Julio Cascalles <jcezarc@yahoo.com.br>
2020-05-20 01:46:07 -0300

    Refactoring...

---
+++ db_defaults.py
@@ -0,0 +1,56 @@
+def default_params(db_type):
+    params = {
+        'dynamodb': {
+            "service_name": 'dynamodb',
+            "region_name": 'us-west-2',
+            "endpoint_url": "http://localhost:8000",
+            "aws_access_key_id": "",
+            "aws_secret_access_key": "",
+            'import_dao_class': 'dynamo_table',
+            'dao_class': 'DynamoTable'
+        },
+        'neo4j': {
+            'host': 'localhost',
+            'port': 7687,
+                    'user': '',
+                    'password': '',
+                    'import_dao_class': 'neo4j_table',
+                    'dao_class': 'Neo4Table'
+        },
+        'postgres': {
+            "dialect": "postgresql",
+            "driver": "psycopg2",
+            "username": "postgres",
+            "password": "xxxxx",
+            "host": "localhost",
+                    "port": "5432",
+                    "database": "pg01",
+                    'import_dao_class': 'sql_table',
+                    'dao_class': 'SqlTable'
+        },
+        'mongodb': {
+            'server': 'mongodb+srv',
+            'host_or_user': '',
+            'port_or_password': '',
+            'database': '',
+            'import_dao_class': 'mongo_table',
+            'dao_class': 'MongoTable'
+        },
+        'sqlserver': {
+            "dialect": "mssql",
+            "driver": "pyodbc",
+            "username": "",
+            "password": "",
+            "host": "localhost",
+                    "port": "1433",
+                    "database": "",
+                    'import_dao_class': 'sql_table',
+                    'dao_class': 'SqlTable'
+        }
+    }
+    result = params.get(db_type.lower(), {})
+    aux = {}
+    if result:
+        for key in ['import_dao_class', 'dao_class']:
+            aux[key] = result.pop(key)
+    return result, aux
--- escher.py
+++ escher.py
@@ -1,16 +1,97 @@
+import os
+import sys
+import json
+import shutil
+from db_defaults import default_params
+
 class BaseGenerator:
-    def render_code(self, params):
-        path = params['path']
-        file_name = params['file_name']
-        write_mode = params.get('write_mode', True)
-        values = params['values']
+    """    
+        1 - Ler JSON
+        2 - Armazenar configurações
+            2.1 - Completar valores ausentes com default.
+        3 - Ler templates e substituir marcas pelas configurações
+            >> 3.1 - Identificar quais arquivos temporários usar em cada pasta;
+            3.2 - Completar nomes de arquivos com nomes de tabelas
+        4 - Salvar os arquivos nas pastas corretas
+            4.1 - Se a pasta não existir, criar
+                4.1.1 - Em pastas vazias existem arquivos obrigatórios?
+                        (__init__.py)
+            4.2 - Remover arquivos desnecessários.
+        5 - Ligar todos os arquivos através de IMPORT
+    """
+
+    def ignore_list(self):
+        db_list = ['dynamo_table.py', 'mongo_table.py', 'neo4j_table.py', 'sql_table.py']
+        dao = self.dao_info['import_dao_class']+'.py'
+        return [i for i in db_list if i != dao]
+
+
+    def __init__(self, json_info):
+        self.tables = json_info['tables']
+        db_type = json_info['db_type']
+        db_config, aux = default_params(db_type)
+        db_config.update(
+            json_info.get('db_config',{})
+        )
+        self.db_config =db_config
+        self.dao_info = aux
+        
+    def create_init_file(self, target):
+        init_file = os.path.join(
+            target,
+            '__init__.py'
+        )
+        with open(init_file, 'w') as f:
+            f.write(' ')
+            f.close()
+
+    def render_code(self, params, read_only=False):
+        path = params.pop('path', '')
+        file_name = params.pop('file_name')
+        origin = os.path.join('templates', path, file_name)
+        target = os.path.join(params["API_name"], path)
+        if not os.path.exists(target):
+            os.makedirs(target)
+            self.create_init_file(target)
+        with open(origin, 'r') as f:
+            text = f.read()
+            f.close()
+        for key in params:
+            value = params[key]
+            ## ??????????????
+            if not isinstance(value, str):
+                continue 
+            ## ??????????????
+            text = text.replace(f'%{key}%', value)
+        if not read_only:
+            target = os.path.join(target, file_name)
+            with open(target, 'w') as f:
+                f.write(text)
+                f.close()
+        return text
+
+    def import_template(self, template_list):
         pass
-    def empty_dir(self, target):
-        pass
-    def create_folders(self):
-        pass
-    def copy_util(self, ignore_list):
-        pass
+
+    def copy_util(self, ref, ignore_list, sub='util'):
+        src = os.path.join('templates', sub)
+        dst = os.path.join(ref, sub)
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+        for f in os.listdir(src):
+            if f in ignore_list:
+                continue
+            s = os.path.join(src, f)
+            d = os.path.join(dst, f)
+            if os.path.isdir(s):
+                self.copy_util(
+                    ref,
+                    ignore_list,
+                    os.path.join(sub, f)
+                )
+                continue
+            shutil.copy2(s, d)
+
     def build_app(self):
         pass
 
