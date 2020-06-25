7ab3f8716415b5d67b51b97e7aed229867d27da0
Julio Cascalles <jcezarc@yahoo.com.br>
2020-06-25 17:19:28 -0300

    Sql objects Refactoring

--- templates/backend/util/db/lite_table.py
+++ templates/backend/util/db/lite_table.py
@@ -1,8 +1,9 @@
 import sqlite3
 import mysql.connector
-from util.db.db_table import DbTable
+from util.db.fmt_table import FormatTable
 
-class LiteTable(DbTable):
+
+class LiteTable(FormatTable):
     def config(self, table_name, schema, params):
         super().config(table_name, schema, params)
         if 'user' in params:
@@ -12,6 +13,9 @@ class LiteTable(DbTable):
 
     def execute(self, command, need_commit):
         cursor = self.connection.cursor()
+        print('-'*100)
+        print(command)
+        print('-'*100)
         cursor.execute(command)
         if need_commit:
             self.connection.commit()
@@ -37,12 +41,6 @@ class LiteTable(DbTable):
             result.append(record)
         return result
 
-    def get_conditions(self, values, only_pk=True):
-        if not values:
-            return ''
-        super().get_conditions(values, only_pk)
-        return ' AND '.join(self.conditions)
-
     def find_one(self, values):
         found = self.find_all(
             1, self.get_conditions(values)
@@ -52,42 +50,28 @@ class LiteTable(DbTable):
         return None
 
     def delete(self, values):
-        command = 'DELETE FROM "{}" WHERE {}'.format(
+        command = 'DELETE FROM {} WHERE {}'.format(
             self.table_name,
             self.get_conditions(values)
         )
         self.execute(command, True)
 
     def insert(self, json_data):
-        errors = self.validator.validate(json_data)
+        errors = super().insert(json_data)
         if errors:
             return errors
-        insert_values = self.statement_columns(
+        command = self.get_command(
             json_data,
-            True,
-            '{value}'
-        )
-        field_list = [field for field in json_data]
-        command = 'INSERT INTO {} ({})VALUES({})'.format(
-            self.table_name,
-            ','.join(field_list),
-            ','.join(insert_values)
+            is_insert=True,
+            use_quotes=False
         )
         self.execute(command, True)
         return None
 
-    @staticmethod
-    def contained_clause(value):
-        return "LIKE '%" + value + "%'"
-
     def update(self, json_data):
-        update_fields = self.statement_columns(
+        command = self.get_command(
             json_data,
-            False
-        )
-        command = 'UPDATE "{}" SET {} WHERE {}'.format(
-            self.table_name,
-            ','.join(update_fields),
-            self.get_conditions(json_data)
+            is_insert=False,
+            use_quotes=False
         )
         self.execute(command, True)
