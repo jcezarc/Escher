6da975aac6707d6af60915916ef2226ce34c30f6
Júlio Cascalles <jcezarc@yahoo.com.br>
2020-10-03 11:33:36 -0300

    Option --jwt

--- lib/jwt_options.py
+++ lib/jwt_options.py
@@ -2,20 +2,20 @@
 def get_jwt_options(empty=False):
     result = {
         'jwt_import_access_token': '''
-            import uuid
-            from flask_jwt_extended import create_access_token, JWTManager
+import uuid
+from flask_jwt_extended import create_access_token, JWTManager
         ''',
         'jwt_secret_key': '''
-            APP.config['JWT_SECRET_KEY'] = str(uuid.uuid4())
-            JWT = JWTManager(APP)
+APP.config['JWT_SECRET_KEY'] = str(uuid.uuid4())
+JWT = JWTManager(APP)
         ''',
         'jwt_requirements': '''
-            Flask-JWT==0.3.2
-            Flask-JWT-Extended==3.24.1
+Flask-JWT==0.3.2
+Flask-JWT-Extended==3.24.1
         ''',
         'jwt_decorator': '@jwt_required',
         'import_jwt_required': '''
-            from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required
         ''',
         'jwt_decorators_list': 'decorators=[jwt_required]',
         'jwt_handshake': '''
